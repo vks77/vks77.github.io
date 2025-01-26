d7f3e388e111163979d0e3f02652aa6dca6e68ec
Vinay Sharma <sharma.vinay1206@gmail.com>
2025-01-26 15:45:47 +0530

    some config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/vks77/vks77.github.io",
+  site: "https://vks77.github.io/",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
