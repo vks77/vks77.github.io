6a1f6eae01dc8187a217aabe6ef0df9cb6b0b08b
Vinay Sharma <sharma.vinay1206@gmail.com>
2025-01-28 08:19:06 +0530

    new icons for page headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vks77.github.io/",
+  site: "https://vksharma.in",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
